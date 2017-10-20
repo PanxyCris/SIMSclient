f0422306f0275f760ecd929bf6e5b82b6ba38066
Your Name <13011204829@163.com>
2017-10-20 21:30:00 +0800

    1

--- src/SIMSclient/src/vo/PromotionVO.java
+++ src/SIMSclient/src/vo/PromotionVO.java
@@ -2,4 +2,17 @@ package SIMSclient.src.vo;
 
 public class PromotionVO {
 
+	public String BillID; // 单据ID
+	public String clientID; // 客户ID
+	public String clientName; // 客户名字
+	public String cellNumber; // 电话号码
+	public String user; // 操作员
+	public String saleMan; // 业务员
+	public double beforePrice; // 折让前价格
+	public double allowance; // 折让金额
+	public double voucher; // 代金券金额
+	public double afterPrice; // 折让后金额
+	public String remark; // 备注
+	 
+
 }
