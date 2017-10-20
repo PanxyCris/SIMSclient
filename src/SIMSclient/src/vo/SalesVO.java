56efa331aefb9716b11de2f2ce9d95de81a5de17
NJULIJIE\id <161250058@smail.nju.edu.cn>
2017-10-20 20:57:38 +0800

    sale

--- src/SIMSclient/src/vo/SalesVO.java
+++ src/SIMSclient/src/vo/SalesVO.java
@@ -1,6 +1,48 @@
 package SIMSclient.src.vo;
 
-public class SalesVO {
+import java.util.ArrayList;
 
+public class SalesVO {
+	public String BillID;   //单据ID
+	public String clientID;   //客户ID
+	public String clientName;   //客户名字
+	public String cellNumber;   //电话号码
+	public String user;  //操作员
+	public String saleMan;  //业务员
+	public double beforePrice;  //折让前价格
+	public double allowance;  //折让金额
+	public double voucher;  //代金券金额
+	public double afterPrice;   //折让后金额
+	public String remark;  //备注
+	public BillType type;   //单据类型
+	public BillState state;  //单据状态
+	public ArrayList<CommodityItemVO> commodities;
+	
+	public SalesVO(String ID, String clientID, String clientName, String cellNumber, String user, String saleMan,
+			double beforePrice, double allowance, double voucher, double afterPrice, String remark, BillType type,
+			BillState state, ArrayList<CommodityItemVO> commodities) {
+		super();
+		this.clientID = clientID;
+		this.clientName = clientName;
+		this.cellNumber = cellNumber;
+		this.user = user;
+		this.saleMan = saleMan;
+		this.beforePrice = beforePrice;
+		this.allowance = allowance;
+		this.voucher = voucher;
+		this.afterPrice = afterPrice;
+		this.remark = remark;
+		this.type = type;
+		this.state = state;
+		this.commodities = commodities;
+	}
+	
+	@Override
+	public String toString() {
+		return "[单据ID=" + ID + "单据类型=" + type.value + ", 客户ID=" + clientID + ", 客户姓名=" + clientName +", 业务员=" + saleMan
+				+ ", 操作员=" + user + ", 商品列表=" + commodities + ",\r\n折让前总额 =" + beforePrice + ", 折让金额=" + allowance
+				+ ", 代金券总额=" + voucher + ", 折让后总额=" + afterPrice + ", 备注=" + remark + ", 单据状态=" + state.value + "]";
+	}
+
+	
 }
-//wwwww33333
