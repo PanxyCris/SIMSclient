e68661ebdb7c7772563fe200efac247e0d8f0034
NJULIJIE\id <161250058@smail.nju.edu.cn>
2017-10-21 14:47:20 +0800

    增加了客户类别与级别的枚举

---
+++ src/SIMSclient/src/vo/bill/ClientCategory.java
@@ -0,0 +1,7 @@
+package SIMSclient.src.vo.bill;
+
+public enum ClientCategory {
+
+	SUPPLIER, //进货商
+	RETAILER  //销售商
+}
---
+++ src/SIMSclient/src/vo/bill/MemberLevel.java
@@ -0,0 +1,5 @@
+package SIMSclient.src.vo.bill;
+
+public enum MemberLevel {
+	LEVEL_ONE, LEVEL_TWO, LEVEL_THREE, LEVEL_FOUR, LEVEL_FIVE
+}
