4d568de711183f29bed7c5dd737d48e829458dc2
Your Name <13011204829@163.com>
2017-10-21 16:02:09 +0800

    promotion2

--- src/SIMSclient/src/bussinesslogicservice/promotionblservice/PromotionBLService.java
+++ src/SIMSclient/src/bussinesslogicservice/promotionblservice/PromotionBLService.java
@@ -7,4 +7,10 @@ import SIMSclient.src.vo.PromotionVO;
 public interface PromotionBLService {
 //dffff
 	public ArrayList<PromotionVO> getCommodityPromotionListByID(int commodityID);
+	public String getBillID(); // 单据ID
+	public String getclientID(); // 客户ID
+	public String getclientName(); // 客户名字
+	public String getcellNumber(); // 电话号码
+	public String getuser(); // 操作员
+	public String getsaleMan(); // 业务员
 }
