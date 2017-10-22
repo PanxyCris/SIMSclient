aec9d1f7b9b43ffa7768322b44e5e6157be59639
Your Name <13011204829@163.com>
2017-10-22 14:34:32 +0800

    promotion stub

---
+++ src/SIMSclient/src/bussiness_stub/PromotionBLService_Stub.java
@@ -0,0 +1,24 @@
+package SIMSclient.src.bussiness_stub;
+
+import SIMSclient.src.vo.PromotionVO;
+
+public class PromotionBLService_Stub {
+	public void getBillID(PromotionVO vo){
+    	System.out.println("getBillID Success.");
+    }
+	public void getclientID(PromotionVO vo){
+    	System.out.println("getclientID Success.");
+    }
+	public void getclientName(PromotionVO vo){
+    	System.out.println("getclientName Success.");
+    }
+	public void getcellNumber(PromotionVO vo){
+    	System.out.println("getcellNumber Success.");
+    }
+	public void getuser(PromotionVO vo){
+    	System.out.println("getuser Success.");
+    }
+	public void getsaleMan(PromotionVO vo){
+    	System.out.println("getsaleMan Success.");
+    }
+}
---
+++ src/SIMSclient/src/data_stub/PromotionDataService_Stub.java
@@ -0,0 +1,20 @@
+package SIMSclient.src.data_stub;
+
+import java.rmi.RemoteException;
+
+import SIMSclient.src.po.PromotionPO;
+
+public class PromotionDataService_Stub {
+	public void getbeforePrice(PromotionPO po) throws RemoteException{
+    	System.out.println("getbeforePrice Success.");
+    }
+	public void getallowance(PromotionPO po) throws RemoteException{
+    	System.out.println("getallowance Success.");
+    }
+	public void getvoucher(PromotionPO po) throws RemoteException{
+    	System.out.println("getvoucher Success.");
+    }
+	public void getafterPrice(PromotionPO po) throws RemoteException{
+    	System.out.println("getafterPrice Success.");
+    }
+}
