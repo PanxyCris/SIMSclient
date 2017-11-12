70f263e418f7a72dd86b67b0c88e916f102aaaec
161250095 <161250095@smail.nju.edu.cn>
2017-11-12 19:55:51 +0800

    改一下bill

--- src/SIMSclient/src/bussinesslogicservice/billblservice/BillBLService.java
+++ src/SIMSclient/src/bussinesslogicservice/billblservice/BillBLService.java
@@ -2,11 +2,13 @@ package SIMSclient.src.bussinesslogicservice.billblservice;
 
 import java.util.Date;
 import SIMSclient.src.vo.bill.BillVO.BillState;
+import SIMSclient.src.vo.bill.BillVO.BillType;
 
 public interface BillBLService {
 
 	public int getID();
 	public Date getDate();
 	public BillState getState();
+	public BillType getType();
 	public String getDescription();
 }
--- src/SIMSserver/src/po/BillPO.java
+++ src/SIMSserver/src/po/BillPO.java
@@ -1,9 +1,20 @@
 package SIMSserver.src.po;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class BillPO {
+public class BillPO implements Serializable{
 
+
+	/**
+	 *
+	 */
+	private static final long serialVersionUID = 1L;
+	private Date date;
+	public int id;
+	BillState state;
+	BillType type;
+	String description;
 
 	public enum BillState {
 		DRAFT("草稿状态"), COMMITED("已提交状态"), SUCCESS("审批成功");
@@ -15,15 +26,32 @@ public class BillPO {
 		}
 	}
 
-	int id;
-	Date date;
-	BillState state;
-	String description;
+	public enum BillType{
+		INVENTORYGIFTBILL("库存赠送单"),INVENTORYLOSSBILL("库存报损单"),INVENTORYREVENUEBILL("库存报溢单"),INVENTORYWARNINGBILL("库存报警单"),
 
-	public BillPO(int i,Date d, BillState s,String des){
+		SALESBILL("销售单"),PURCHASEBILL("进货单"),SALESBACKBILL("销售退货单"),PURCHASEBACKBILL("进货退货单"),
+
+		SKD("收款单"),XJFYD("付款单");
+
+		public final String value;
+
+		BillType(String value){
+			this.value = value;
+		}
+	}
+
+
+	/**
+	 * @param type
+	 * @param state
+	 * @param date
+	 */
+	public BillPO(int i,Date d,BillState s, BillType t,String des) {
+
 		id = i;
 		date = d;
 		state = s;
+		type = t;
 		description = des;
 	}
 
@@ -31,16 +59,22 @@ public class BillPO {
 		return id;
 	}
 
-	public Date getDate(){
+	public Date getDate() {
 		return date;
 	}
-
 
 	public BillState getState(){
 		return state;
 	}
 
+	public BillType getType(){
+		return type;
+	}
+
 	public String getDescription(){
 		return description;
 	}
+
+
+
 }
