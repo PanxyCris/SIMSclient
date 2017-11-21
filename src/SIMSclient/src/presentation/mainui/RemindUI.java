35228717af473b2ecbc8d5b198beb60f1690c433
161250095 <161250095@smail.nju.edu.cn>
2017-11-21 21:11:39 +0800

    make some changes

--- src/SIMSclient/src/presentation/mainui/RemindUI.java
+++ src/SIMSclient/src/presentation/mainui/RemindUI.java
@@ -5,16 +5,31 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
 public class RemindUI {
 
 	@FXML
 	Button button;
+	@FXML
+	AnchorPane pane;
 
 	@FXML
 	public void confirm(){
-	    Stage stage = (Stage) button.getScene().getWindow();
+	   exit();
+	}
+	
+	@FXML
+	public void exitKey(KeyEvent event){
+		if(event.getCode()==KeyCode.ENTER)
+			exit();
+	}
+	
+	public void exit(){
+		Stage stage = (Stage) button.getScene().getWindow();
 	    stage.close();
 	}
 
