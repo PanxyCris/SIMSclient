770c7457de7c98b98158459ac811e03a6750556c
user <user@172.28.136.135>
2017-11-26 10:31:40 +0800

    no message

--- src/SIMSclient/src/bussinesslogic/accountbl/AccountBL.java
+++ src/SIMSclient/src/bussinesslogic/accountbl/AccountBL.java
@@ -2,12 +2,36 @@ package SIMSclient.src.bussinesslogic.accountbl;
 
 import SIMSclient.src.bussinesslogicservice.accountblservice.AccountBLService;
 import SIMSclient.src.dataenum.ResultMessage;
+import SIMSclient.src.dataservice.accountdataservice.AccountDataService;
+import SIMSclient.src.po.AccountPO;
 import SIMSclient.src.vo.AccountVO;
 
+/**
+ * 
+ * @author Íõ²Ó²Ó
+ * @version 2017-10-26
+ *
+ */
 public class AccountBL implements AccountBLService{
-
+	
+	private static AccountBL accountBL=new AccountBL();
+	
+	public AccountBL getInstance(){
+		return accountBL;
+	}
+	
+	public 
+	
+	AccountVO accountVO=new AccountVO("",0);
+	AccountDataService accountDataService;
+	
 	public AccountVO find(String message) {
-		return null;
+		
+		accountVO
+		
+	
+		
+		return accountVO;
 	}
 
 	public ResultMessage newBuild() {
--- src/SIMSserver/src/data/accountdata/AccountDataServiceMySqlImpl.java
+++ src/SIMSserver/src/data/accountdata/AccountDataServiceMySqlImpl.java
@@ -1,5 +1,52 @@
 package SIMSserver.src.data.accountdata;
 
-public class AccountDataServiceMySqlImpl {
+import SIMSclient.src.dataenum.ResultMessage;
+import SIMSclient.src.po.AccountPO;
+import SIMSserver.src.dataservice.accountdataservice.AccountDataService;
+
+/**
+ * 
+ * @author Íõ²Ó²Ó
+ * @version 2017-11-26
+ * 
+ */
+public class AccountDataServiceMySqlImpl implements AccountDataService{
+	
+	private static AccountDataServiceMySqlImpl accountDataServiceMySqlImpl=new AccountDataServiceMySqlImpl();
+
+	public AccountDataServiceMySqlImpl getInstance(){
+		return accountDataServiceMySqlImpl;
+	}
+	
+	public AccountDataServiceMySqlImpl() {
+	}
+	
+	public AccountDataService getService(){
+		return (AccountDataService)accountDataServiceMySqlImpl;
+	}
+	
+	@Override
+	public AccountPO find(String message) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultMessage newBuild() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultMessage delete(AccountPO accountPO) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ResultMessage modify(String name) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
