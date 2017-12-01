93a05b9bc22b482d0d69bc06e6c3c4146e1d2061
161250095 <161250095@smail.nju.edu.cn>
2017-12-01 16:16:24 +0800

    AccountManageUI完成

--- src/SIMSclient/src/presentation/accountui/AccountFindUI.java
+++ src/SIMSclient/src/presentation/accountui/AccountFindUI.java
@@ -1,5 +1,51 @@
 package SIMSclient.src.presentation.accountui;
 
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.ResourceBundle;
+
+import SIMSclient.src.presentation.remindui.RemindPrintUI;
+import SIMSclient.src.vo.AccountVO;
+import javafx.application.Platform;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.stage.Stage;
+
 public class AccountFindUI extends AccountManageUI{
+
+	@FXML
+	public void nameFind(){
+	       ArrayList<AccountVO> accountList = service.find(findingField.getText());
+	       if(list==null){
+	    	   Platform.runLater(new Runnable() {
+		    	    public void run() {
+		    	        try {
+		    	        	new RemindPrintUI().start(new Stage());
+						} catch (Exception e) {
+							e.printStackTrace();
+						}
+		    	    }
+		    	});
+	       }
+	       else{
+	    	   ObservableList<AccountVO> newList = FXCollections.observableArrayList();
+	    	   newList.addAll(accountList);
+	    	   table.setItems(newList);
+	       }
+
+	}
+
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		findingField.setText(null);
+	}
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		changeStage("UserFindUI","UserFindUI.fxml");
+	}
+
 
 }
