20074172aa1ccdf5ca9481c07c9e6ac654d837cf
Your Name <13011204829@163.com>
2017-12-11 14:53:01 +0800

    修改po

--- SIMSclient/src/po/PromotionPO.java
+++ SIMSclient/src/po/PromotionPO.java
@@ -7,6 +7,16 @@ public class PromotionPO extends PersistObject{
 	public PromotionPO(String id) {
 		super(id);
 	}
-
+	public String BillID; // 单据ID
+	public String clientID; // 客户ID
+	public String clientName; // 客户名字
+	public String cellNumber; // 电话号码
+	public String user; // 操作员
+	public String saleMan; // 业务员
+	public double beforePrice; // 折让前价格
+	public double allowance; // 折让金额
+	public double voucher; // 代金券金额
+	public double afterPrice; // 折让后金额
+	public String remark; // 备注
 	
 }
