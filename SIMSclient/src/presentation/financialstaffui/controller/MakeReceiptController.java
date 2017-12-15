3129845a94cb3d9e68c575a83f859c1439f30bac
161250095 <161250095@smail.nju.edu.cn>
2017-12-15 13:35:59 +0800

    对账户界面的一些修改

---
+++ SIMSclient/src/presentation/financialstaffui/controller/MakeReceiptController.java
@@ -0,0 +1,44 @@
+package presentation.financialstaffui.controller;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import dataenum.BillType;
+import dataenum.Remind;
+import javafx.collections.FXCollections;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ChoiceBox;
+import vo.UserVO;
+
+public class MakeReceiptController extends FinancialStaffController implements Initializable{
+
+	public static final Remind remind = Remind.BILL;
+	UserVO user;
+	@FXML
+	ChoiceBox<String> receiptChoice;
+
+	@FXML
+	public void chooseReceipt() throws Exception{
+		BillType type = BillType.getType(receiptChoice.getValue());
+		switch(type){
+		case SKD:changeStage("ReceiveMakeBillUI",user);break;
+		case XJFYD:changeStage("PaymentMakeBillUI",user);break;
+		default:break;
+		}
+	}
+
+	public void initData(UserVO user) throws Exception {
+           this.user = user;
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		choiceInit();
+	}
+
+	public void choiceInit(){
+		receiptChoice.setItems(FXCollections.observableArrayList(BillType.SKD.value,BillType.XJFYD.value));
+	}
+
+}
