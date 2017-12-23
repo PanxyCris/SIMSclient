db2270a802b0e40a1532fd67a25d274a760c00b7
Your Name <13011204829@163.com>
2017-12-23 22:31:22 +0800

    promotionbl

--- SIMSclient/src/bussinesslogic/promotionbl/PromotionBL.java
+++ SIMSclient/src/bussinesslogic/promotionbl/PromotionBL.java
@@ -1,6 +1,8 @@
 package bussinesslogic.promotionbl;
 
+import java.rmi.RemoteException;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import bussinesslogicservice.promotionblservice.PromotionBLService;
 import dataenum.ResultMessage;
@@ -9,39 +11,47 @@ import vo.promotion.PromotionVO;
 
 public class PromotionBL implements PromotionBLService{
 //1234567
-
-		private static PromotionBL promotionBL=new PromotionBL();
-		ResultMessage resultMessage = null;
-		
-		PromotionVO promotionVO;
-		PromotionPO promotionPO;
-		@Override
-		public ArrayList<vo.PromotionVO> getCommodityPromotionListByID(int commodityID) {
-			return null;
+	
+	@Override
+	public ResultMessage delete(String id) throws RemoteException{
+		return service.deletePromotion(id);
+	}
+	@Override
+	public ResultMessage update(MemberVO vo) throws RemoteException{
+		return service.updatePromotion(transition.VOtoPO(vo));
+	}
+	@Override
+	public getId() throws RemoteException {
+		ArrayList<PromotionPO> list = service.showPromotion();
+		ArrayList<Integer> IDList = new ArrayList<>();
+		for (PromotionPO po : list) {
+			IDList.add(Integer.parseInt(po.getId()));
 		}
-		@Override
-		public String getBillID() {
-			return null;
+		Collections.sort(IDList);
+		String id = String.valueOf(IDList.get(IDList.size()-1)+1);
+		StringBuilder sb = new StringBuilder(id);
+		int len = id.length();
+		for (int i=0; i < 6-len; i++) {
+			sb.insert(0, "0");
 		}
-		@Override
-		public String getclientID() {
-			return null;
+		return sb.toString();
+	}
+	@Override
+	public delete() throws RemoteException{
+		return service.deletePromotion();
+	}
+	@Override
+	public ArrayList<PromotionVO> find(String info, FindPromotionType findType) throws RemoteException {
+		ArrayList<PromotionVO> list = new ArrayList<>();
+		ArrayList<PromotionPO> poList = service.findPromotion(keyword, type);
+		PromotionVO vo = null;
+		for (PromotionPO po : poList) {
+			vo = transition.POtoVO(po);
+			list.add(vo);
 		}
-		@Override
-		public String getclientName() {
-			return null;
-		}
-		@Override
-		public String getcellNumber() {
-			return null;
-		}
-		@Override
-		public String getuser() {
-			return null;
-		}
-		@Override
-		public String getsaleMan() {
-			return null;
-		} 
-
+		return list;
+	}
+	public ArrayList<P> getPromotionList(){
+		return service.promotionList();
+	}
 }
