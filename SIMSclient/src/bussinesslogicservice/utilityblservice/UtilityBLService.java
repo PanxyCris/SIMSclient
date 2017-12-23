a0a5f1f86299676a90b83e9ce0c0b1deae897465
PanxyCris <739312347@qq.com>
2017-12-23 17:18:39 +0800

    一点注释

--- SIMSclient/src/bussinesslogicservice/utilityblservice/UtilityBLService.java
+++ SIMSclient/src/bussinesslogicservice/utilityblservice/UtilityBLService.java
@@ -11,9 +11,17 @@ public interface UtilityBLService {
 	public boolean isPrintLegal(ArrayList<String> list);
 
 	public String generateID(BillType billType);
-
+    /**
+     * 通过用户信息得到该用户的最新信息
+     * @param user
+     * @return
+     */
 	public ArrayList<MessageVO> getMessage(UserVO user);
-
+   /**
+    * 判断给用户是否拥有最新信息
+    * @param user
+    * @return
+    */
 	public boolean hasMessage(UserVO user);
 
 }
