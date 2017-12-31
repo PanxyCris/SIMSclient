aea342cdc7f373c93c30a4b3f553b156d1ced73b
user <user@172.28.166.166>
2017-12-31 09:47:14 +0800

    try

--- SIMSclient/src/bussinesslogic/accountbl/AccountTransition.java
+++ SIMSclient/src/bussinesslogic/accountbl/AccountTransition.java
@@ -8,18 +8,15 @@ public class AccountTransition {
 	public AccountPO VOtoPO(AccountVO accountVO){
 		String id=accountVO.getId();
 		String name=accountVO.getName();
-		Double money=Double.valueOf(accountVO.getMoney());
-		
-		AccountPO accountPO=new AccountPO(id, name, money);
-		return accountPO;
+		Double money=accountVO.getMoney();
+		return new AccountPO(id, name, money);
 	}
 	
 	public AccountVO POtoVO(AccountPO accountPO){
 		String id=accountPO.getId();
 		String name=accountPO.getName();
 		Double money=accountPO.getMoney();
-		
-		AccountVO accountVO=new AccountVO(id, name, money);
-		return accountVO;
+		return new AccountVO(id, name, money);
 	}
+	
 }
