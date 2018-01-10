f000ec4349511469aaaa3c1ed669ce51b7baf0fb
161250095 <161250095@smail.nju.edu.cn>
2018-01-10 21:36:23 +0800

    make some chanegs

--- SIMSserver/src/po/AccountBookPO.java
+++ SIMSserver/src/po/AccountBookPO.java
@@ -1,19 +1,63 @@
 package po;
 
+import java.util.ArrayList;
+
+import po.commodity.CommodityPO;
+
 public class AccountBookPO extends PersistObject {
 	private static final long serialVersionUID = 1L;
-	private String clerkID;
+	private String date;
+	private String clerkID;//操作员ID
+	private ArrayList<CommodityPO> commodityPOs;//商品列表
+	private ArrayList<MemberPO> memberPOs;//客户列表
+	private ArrayList<AccountPO> accountPOs;//账户列表
 
-	public AccountBookPO(String id, String c) {
-		super(id);
-		clerkID = c;
+	public AccountBookPO(String date, String clerkID,ArrayList<CommodityPO> commodityPOs,ArrayList<MemberPO> memberPOs
+			,ArrayList<AccountPO> accountPOs) {
+		this.date=date;
+		this.clerkID=clerkID;
+		this.commodityPOs=commodityPOs;
+		this.memberPOs=memberPOs;
+		this.accountPOs=accountPOs;
+	}
+
+	public String getDate() {
+		return date;
+	}
+
+	public void setDate(String date) {
+		this.date = date;
 	}
 
 	public String getClerkID() {
-		return this.clerkID;
+		return clerkID;
 	}
 
 	public void setClerkID(String clerkID) {
 		this.clerkID = clerkID;
+	}
+
+	public ArrayList<CommodityPO> getCommodityPOs() {
+		return commodityPOs;
+	}
+
+	public void setCommodityPOs(ArrayList<CommodityPO> commodityPOs) {
+		this.commodityPOs = commodityPOs;
+	}
+
+	public ArrayList<MemberPO> getMemberPOs() {
+		return memberPOs;
+	}
+
+	public void setMemberPOs(ArrayList<MemberPO> memberPOs) {
+		this.memberPOs = memberPOs;
+	}
+
+	public ArrayList<AccountPO> getAccountPOs() {
+		return accountPOs;
+	}
+
+	public void setAccountPOs(ArrayList<AccountPO> accountPOs) {
+		this.accountPOs = accountPOs;
 	}
 }
