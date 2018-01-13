2c97bc885fc00a6f6ddd436cf9b0b89260bc25ab
161250095 <161250095@smail.nju.edu.cn>
2018-01-13 13:13:04 +0800

    cdas

--- SIMSclient/src/main/java/presentation/generalmanagerui/CheckPaymentBillUI.java
+++ SIMSclient/src/main/java/presentation/generalmanagerui/CheckPaymentBillUI.java
@@ -1,15 +1,10 @@
 package presentation.generalmanagerui;
 
-import dataenum.BillType;
 import javafx.fxml.FXMLLoader;
-
 import javafx.scene.Scene;
 import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 import presentation.generalmanagerui.controller.CheckPaymentBillController;
-import presentation.generalmanagerui.controller.ExamineBillController;
-import presentation.generalmanagerui.controller.ExaminePaymentBillController;
-import presentation.generalmanagerui.controller.GeneralManagerController;
 import vo.uservo.UserVO;
 
 public class CheckPaymentBillUI{
